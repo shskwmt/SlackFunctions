ef4e4ef8580e383643579f2294aac5d4db971f7e
shskwmt <shskwmt@outlook.jp>
2020-02-17 23:22:39 +0900

    Add some functions

---
+++ SlackFunctions.psm1
@@ -0,0 +1,89 @@
+###
+# Author: shskwmt
+###
+
+Function Get-SlackMentions {
+  <#
+  .SYNOPSIS
+    Get slack mentions for me.
+  #>
+  Param (
+    [Parameter(Mandatory)][String]$Workspace,
+    [Parameter(Mandatory)][String]$Token,
+    [Parameter(Mandatory)][String]$AuthToken,
+    [ValidateRange(1,1000)][Int]$Count = 100
+  )
+
+  $mentionsUri = 'https://' + $Workspace + '.slack.com/api/activity.mentions'
+  $mentionsParameters = @{
+    token = $Token
+    count = $Count
+  }
+
+  $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+  $cookie = New-Object System.Net.Cookie
+  $cookie.Name = 'd'
+  $cookie.Value = $AuthToken
+  $cookie.Domain = '.slack.com'
+  $session.Cookies.Add($cookie);
+
+  $mentionsResponse = Invoke-WebRequest -Uri $mentionsUri -WebSession $session -Body $mentionsParameters
+  return ConvertFrom-Json $mentionsResponse.Content
+}
+
+Function Get-SlackStarredMessages {
+  <#
+  .SYNOPSIS
+    Get slack mentions for me.
+  #>
+  Param (
+    [Parameter(Mandatory)][String]$Workspace,
+    [Parameter(Mandatory)][String]$Token,
+    [Parameter(Mandatory)][String]$AuthToken,
+    [ValidateRange(1,1000)][Int]$Count = 100
+  )
+
+  $starsUri = 'https://' + $Workspace + '.slack.com/api/stars.list'
+  $starsParameters = @{
+    token = $Token
+    count = $Count
+    exclide = @('Ch', 'Gh', 'Dh')
+  }
+
+  $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+  $cookie = New-Object System.Net.Cookie
+  $cookie.Name = 'd'
+  $cookie.Value = $AuthToken
+  $cookie.Domain = '.slack.com'
+  $session.Cookies.Add($cookie);
+
+  $starsResponse = Invoke-WebRequest -Uri $starsUri -WebSession $session -Body $starsParameters
+  return ConvertFrom-Json $starsResponse.Content
+}
+
+Function Get-SlackUsers {
+  <#
+  .SYNOPSIS
+    Get slack users.
+  #>
+  Param (
+    [Parameter(Mandatory)][String]$Workspace,
+    [Parameter(Mandatory)][String]$Token,
+    [Parameter(Mandatory)][String]$AuthToken
+  )
+
+  $usersUri = 'https://' + $Workspace + '.slack.com/api/users.list'
+  $usersParameters = @{
+    token = $Token
+  }
+
+  $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+  $cookie = New-Object System.Net.Cookie
+  $cookie.Name = 'd'
+  $cookie.Value = $AuthToken
+  $cookie.Domain = '.slack.com'
+  $session.Cookies.Add($cookie);
+
+  $usersResponse = Invoke-WebRequest -Uri $usersUri -WebSession $session -Body $usersParameters
+  return ConvertFrom-Json $usersResponse.Content
+}
